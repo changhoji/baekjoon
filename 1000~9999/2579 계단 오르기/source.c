45fa5ef58c53d084c7431d752b78a7d83c1eeab8
onkkno <onkkno1@gmail.com>
2021-07-29 20:26:40 +0900

    ADD: BOJ 11726번 2xn 타일링
    dp

---
+++ 1000~9999/2579 계단 오르기/source.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int dp[301];
+    int score[301] = { 0,};
+    
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &score[i]);
+    }
+
+    
+    dp[1] = score[1];
+    dp[2] = score[1] + score[2];
+    dp[3] = max(score[1] + score[3], score[2] + score[3]);
+    
+    for (int i = 4; i <= n; i++) {
+        dp[i] = max(dp[i-3] + score[i-1] + score[i], dp[i-2] + score[i]);
+    }
+
+    printf("%d\n", dp[n]);
+
+    return 0;
+}
