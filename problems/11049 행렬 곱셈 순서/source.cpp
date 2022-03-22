01bb4e109f3ff143bb45ed742484d9f5cad22a54
onkkno <onkkno1@gmail.com>
2022-03-23 01:12:09 +0900

    Update source.cpp

--- problems/11049 행렬 곱셈 순서/source.cpp
+++ problems/11049 행렬 곱셈 순서/source.cpp
@@ -15,21 +15,21 @@ int main() {
     }
 
     int dp[501][501];
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            dp[i][j] = 1;
-        }
-    }
+
 
     for (int section_size = 1; section_size <= N-1; section_size++) {
         for (int left = 1; left <= N-section_size; left++) {
             int right = section_size + left;
 
-            dp[left][right] = min(dp[left][right-1] + s[left].first*s[right].first*s[right].second,
-                                  dp[left+1][right] + s[left].first*s[left].second*);
+            for (int connection = left; connection <= right; connection++) {
+                if (connection == left) dp[left][right] = dp[left][connection] + dp[connection+1][right] + s[left].first*s[connection].second*s[right].second;
+                else dp[left][right] = min(dp[left][right], dp[left][connection] + dp[connection+1][right] + s[left].first*s[connection].second*s[right].second);
+            }
         }
     }
 
+    cout << dp[1][N] << endl;
+
+    return 0;
 }
 
-dp[i]: i번째까지 계산할때 연산수
