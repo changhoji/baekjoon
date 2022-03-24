5999faee7e6a4d04c3bf679af172d7510b610250
2021035487 <onkkno1@gmail.com>
2022-03-24 12:34:11 +0900

    Create source.cpp

---
+++ problems/20295 사탕 배달/source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+
+int shop[100001];
+
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> shop[i];
+    }
+
+    
+}
