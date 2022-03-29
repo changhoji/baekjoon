0ac8faf0a39c385174ee527b2f7c2793f39fa740
onkkno <onkkno1@gmail.com>
2022-03-30 01:34:38 +0900

    Update source.cpp

--- problems/9660 돌 게임 6/source.cpp
+++ problems/9660 돌 게임 6/source.cpp
@@ -6,8 +6,21 @@ typedef long long ll;
 int main() {
     ll n;
     cin >> n;
-    
-    
+
+    n %= 8;
+
+    switch (n) {
+        case 1: //a:1
+        case 3: //a:3
+        case 4: //a:4
+        case 5: //a:3 -> b:1 -> a:1
+        case 6: //a:4 -> b:1 -> a:1
+            cout << "SK" << endl;
+            break;
+        default:
+            cout << "CY" << endl;
+            break;
+    }
 
     return 0;
 }
