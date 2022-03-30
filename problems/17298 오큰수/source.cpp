5e1846c1063cd103a9cf37ab5ec71e3b8372ef2b
2021035487 <onkkno1@gmail.com>
2022-03-30 16:35:33 +0900

    yet

---
+++ problems/17298 오큰수/source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int A[1000000];
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    
+}
