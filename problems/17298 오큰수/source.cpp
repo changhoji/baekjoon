41fd672ac44fe7011a90de45851f6e93f5b5989a
2021035487 <onkkno1@gmail.com>
2022-03-31 11:43:01 +0900

    AC

--- problems/17298 오큰수/source.cpp
+++ problems/17298 오큰수/source.cpp
@@ -2,13 +2,36 @@
 #include <stack>
 using namespace std;
 
+int NGE[1000001];
+int num[1000001];
+
 int main() {
     int N;
     cin >> N;
-    int A[1000000];
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];
+    
+    
+    for (int i = 1; i <= N; i++) {
+        NGE[i] = -1;
+        cin >> num[i];
     }
 
-    
+    stack<int> s; //first: idx, second: num
+
+    for (int i = N; i > 0; i--) {
+        while (!s.empty() && s.top() <= num[i]) {
+            s.pop();
+        }
+
+        if (!s.empty()) {
+            NGE[i] = s.top();
+        }
+        s.push(num[i]);
+    }
+
+    for (int i = 1; i <= N; i++) {
+        cout << NGE[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
