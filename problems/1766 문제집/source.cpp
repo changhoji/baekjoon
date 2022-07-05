49525d4e8b164a38733aae7a45ea79562e1b516f
2021035487 <onkkno1@gmail.com>
2022-07-05 13:52:53 +0900

    AC

---
+++ problems/1766 문제집/source.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int degree[32001] = { 0,};
+vector<int> adj[32001];
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        adj[a].push_back(b);
+        degree[b]++;        
+    }
+
+    priority_queue<int, vector<int>, greater<int>> q;
+
+    for (int i = 1; i <= N; i++) {
+        if (degree[i] == 0) q.push(i);
+    }
+
+    for (int i = 1; i <= N; i++) {
+        int cur = q.top();
+        q.pop();
+
+        cout << cur << " ";
+
+        for (auto n: adj[cur]) {
+            degree[n]--;
+            if (degree[n] == 0) q.push(n);
+        }
+    }
+
+    cout << '\n';
+
+    return 0;
+}
